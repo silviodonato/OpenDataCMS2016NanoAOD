e01e26dd2b4f7867d5e5cd4211a944836c4cb86f
silviodonato <silvio.donato@cern.ch>
2024-05-16 15:38:06 +0200

    cleaning

--- example.py
+++ example.py
@@ -8,28 +8,14 @@ sys.path.append(os.path.abspath(os.path.curdir))  # append current dir explicit 
 from MyAnalysis import MyAnalysis
 from Plotter import plotVar, plotVarNorm, plotShapes
 
-### Instantiation of an object of kind MyAnalysis for each single sample
-# TT = MyAnalysis("ttbar")
-# TT.processEvents()
 
 lumi =  8892.1 * 289101.00/1.7402106e+08 # pb-1 (xsec in pb) 
 ## Run2016G 7625.376423
 ## Run2016H 8892.128926
 
+### Instantiation of an object of kind MyAnalysis for each single sample
 DY = MyAnalysis("dy", 6529.0, lumi, maxEvents=200000 )
 DY.processEvents()
-
-# QCD = MyAnalysis("qcd")
-# QCD.processEvents()
-
-# SingleTop = MyAnalysis("single_top")
-# SingleTop.processEvents()
-
-# WJets = MyAnalysis("wjets")
-# WJets.processEvents()
-
-# WW = MyAnalysis("ww")
-# WW.processEvents()
 
 ggH4L = MyAnalysis("ggH4L", 28.87*0.001, lumi, maxEvents=-2000 )
 ggH4L.processEvents()
@@ -37,13 +23,10 @@ ggH4L.processEvents()
 ZZ = MyAnalysis("zz", 12.14, lumi, maxEvents=-2000 )
 ZZ.processEvents()
 
-# WZ = MyAnalysis("wz")
-# WZ.processEvents()
-
 Data = MyAnalysis("Run2016H", -1 , -1, maxEvents=-2000)
 Data.processEvents()
 
-# samples = ["qcd", "zz", "wz", "ww", "single_top", "dy", "wjets", "ttbar"]
+## samples to be processed
 samples = ["ggH4L","zz", "dy"]
 
 variables = ["NIsoMu", "Muon_Pt","Muon_Iso"]
